404f3439fab67e79bd2170bb86fa056621822413
wangyongpeng <wangyongpeng@alibaba-inc.com>
2017-12-21 16:24:19 +0800

    init

--- src/main/java/com/rexwong/concurrent/MyCurrentDemo.java
+++ src/main/java/com/rexwong/concurrent/MyCurrentDemo.java
@@ -5,7 +5,19 @@ import java.util.concurrent.Executors;
 
 public class MyCurrentDemo {
     public static void main(String[] args) {
-        Executor executor= Executors.newFixedThreadPool(1);
+//        baseDemo();
+    }
+
+    private static void fixedPoolDemo() {
+        Executor executor= Executors.newFixedThreadPool(1);//等同于newSingleThreadExecutor()
+        executor.execute(new Runnable(){
+            public void run() {
+                System.out.print("thread1");
+            }
+        });
+    }
+    private static void schedulePoolDemo() {
+        Executor executor= Executors.newScheduledThreadPool(1);
         executor.execute(new Runnable(){
             public void run() {
                 System.out.print("thread1");
--- src/main/java/com/rexwong/concurrent/current.md
+++ src/main/java/com/rexwong/concurrent/current.md
@@ -16,3 +16,23 @@
 
 ## 线程状态
 
+1. **RUNNING**:  
+
+   > Accept new tasks and process queued tasks
+
+2. **SHUTDOWN**: 
+
+   > Don't accept new tasks, but process queued tasks
+
+3. **STOP**:     
+
+   > Don't accept new tasks, don't process queued tasks,and interrupt in-progress tasks
+
+4. **TIDYING**: 
+
+   > All tasks have terminated, workerCount is zero,the thread transitioning to state TIDYING will run the terminated() hook method
+
+5. **TERMINATED**:
+
+   > ==terminated()== has completed
+
