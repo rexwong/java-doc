484ba6a21770892344536022dd0625a5f5b94349
wangyongpeng <wangyongpeng@alibaba-inc.com>
2017-12-21 14:12:40 +0800

    init

---
+++ src/main/java/com/rexwong/current/MyCurrentDemo.java
@@ -0,0 +1,15 @@
+package com.rexwong.current;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MyCurrentDemo {
+    public static void main(String[] args) {
+        Executor executor= Executors.newFixedThreadPool(1);
+        executor.execute(new Runnable(){
+            public void run() {
+                System.out.print("thread1");
+            }
+        });
+    }
+}
