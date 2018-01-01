61f19c2d2eb1610da5f066af3b790140058552e5
rexwong <yongpeng.wong@gmail.com>
2018-01-01 15:13:35 +0800

    jvm init

--- src/main/java/com/rexwong/beans/JavaBeansDemo.java
+++ src/main/java/com/rexwong/beans/JavaBeansDemo.java
@@ -1,22 +1,23 @@
 package com.rexwong.beans;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.MethodDescriptor;
+import java.beans.*;
 import java.util.stream.Stream;
 
 /**
  * java beans 内省模式
  */
 public class JavaBeansDemo {
-    public static void main(String[] args) {
-        try {
-            BeanInfo beanInfo = Introspector.getBeanInfo(User.class);
-            MethodDescriptor[] methodDescriptors = beanInfo.getMethodDescriptors();
-            Stream.of(methodDescriptors).forEach(System.out::println);
-        } catch (IntrospectionException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IntrospectionException {
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(User.class);
+
+        BeanDescriptor beanDescriptor = beanInfo.getBeanDescriptor();
+        System.out.println(beanDescriptor);
+
+        MethodDescriptor[] methodDescriptors = beanInfo.getMethodDescriptors();
+        Stream.of(methodDescriptors).forEach(System.out::println);
+
+        PropertyDescriptor[] propertyDescriptors =  beanInfo.getPropertyDescriptors();
+        Stream.of(propertyDescriptors).forEach(System.out::println);
     }
 }
