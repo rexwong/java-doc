a57a3c4317274f650d2d62d79920659f45387706
rexwong <yongpeng.wong@gmail.com>
2018-01-01 15:30:16 +0800

    java beans 内省

--- src/main/java/com/rexwong/beans/JavaBeansDemo.java
+++ src/main/java/com/rexwong/beans/JavaBeansDemo.java
@@ -8,7 +8,23 @@ import java.util.stream.Stream;
  */
 public class JavaBeansDemo {
     public static void main(String[] args) throws IntrospectionException {
+//        userBeanDescriptor();
+        sunBeanDescritptor();
 
+    }
+    private static void sunBeanDescritptor() throws IntrospectionException{
+        //多例---》软引用、弱引用成员方便GC
+        //多例---》每次获得都内省一次
+        BeanInfo beanInfo = Introspector.getBeanInfo(SonUser.class,Object.class);
+        BeanDescriptor beanDescriptor = beanInfo.getBeanDescriptor();
+        System.out.println(beanDescriptor);
+
+        MethodDescriptor[] methodDescriptors = beanInfo.getMethodDescriptors();
+        Stream.of(methodDescriptors).forEach(System.out::println);
+    }
+    private static void userBeanDescriptor() throws IntrospectionException {
+        //多例---》软引用、弱引用成员方便GC
+        //多例---》每次获得都内省一次
         BeanInfo beanInfo = Introspector.getBeanInfo(User.class);
 
         BeanDescriptor beanDescriptor = beanInfo.getBeanDescriptor();
---
+++ src/main/java/com/rexwong/beans/SonUser.java
@@ -0,0 +1,22 @@
+package com.rexwong.beans;
+
+public class SonUser extends User{
+    private int age;
+    private String sex;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
